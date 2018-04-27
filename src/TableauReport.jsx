f1b411a6797344f3d29b9a825bd1a724c5a1b82a
Mevin <mevinbabuc@gmail.com>
2018-04-27 16:06:28 +0530

    Update TableauReport.jsx

--- src/TableauReport.jsx
+++ src/TableauReport.jsx
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import url from 'url';
 import { Promise } from 'es6-promise';
 import shallowequal from 'shallowequal';
